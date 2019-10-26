44f3eb5ce25157375604b74fdce1f47861f04b1f
Alejandro Varona <avarona10@gmail.com>
2019-10-26 18:11:36 -0400

    memoize main function from request

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,18 @@
+const memoize = (func: (n: number) => void) => {
+  let cache = {} as any;
+
+  return (n: number) => {
+    if(cache[n]) {
+      console.log('Retrieve from Cache...');
+      return cache[n];
+    } else {
+      console.log('Calculate result...');
+      cache[n] = func(n);
+      return cache[n];
+    }
+  }
+}
+
 const sumOfSquares = (n: number) => {
   let sum = 0;
   for(let i = 1; i <= n; i++) {
@@ -18,12 +33,14 @@ const calculateDifference = (n: number) => {
   return Math.abs(squareOfSum(n) - sumOfSquares(n));
 };
 
+const memoCalculateDifference = memoize(calculateDifference);
+
 export const getDifferenceOfSquares = (n: number) => {
   return new Promise((resolve, reject) => {
     try {
       resolve({
         datetime: new Date().toString(), //current date/time of this request,
-        value: calculateDifference(n), // solution,
+        value: memoCalculateDifference(n), // solution,
         number: n, // input number
         occurrences: '',  // the number of times input has been requested
         last_datetime: '', // last date/time of request for this input
