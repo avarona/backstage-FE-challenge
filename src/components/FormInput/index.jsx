7be2c1c7e0607fc6985350fcc2ddddada8072ed7
Alejandro Varona <avarona10@gmail.com>
2019-10-26 12:04:25 -0400

    reorganize into containers & components

---
+++ src/components/FormInput/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const FormInput = () => (
+  <div className="input-container">
+    <input className="input" type="text" />
+  </div>
+);
+
+export default FormInput;
---
+++ src/containers/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+import FormInput from '../../components/FormInput';
+
+const App = () => (
+  <div className="App-container">
+    <FormInput />
+  </div>
+);
+
+export default App;
